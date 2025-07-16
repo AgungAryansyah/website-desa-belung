d4e835437676ba2dfdd18f9867ede1a4d3de5c50
aharismuhasibi <66463824+Hmuhasibi21@users.noreply.github.com>
2025-07-16 10:31:11 +0700

    feat: home berita

--- src/templates/PageTemplate.js
+++ src/templates/PageTemplate.js
@@ -15,18 +15,9 @@ export default function PageTemplate({
     <div className="min-h-screen flex flex-col">
       <Header />
       
-      <main className={`flex-1 pt-20 ${className}`}>
+      <main className={'bg-gray-100'}>
         {/* Page Header */}
-        {title && (
-          <div className="bg-gray-50 py-12">
-            <div className="container mx-auto px-4">
-              <h1 className="text-4xl font-bold text-gray-800 mb-2">{title}</h1>
-              {description && (
-                <p className="text-gray-600 text-lg">{description}</p>
-              )}
-            </div>
-          </div>
-        )}
+        
         
         {/* Page Content */}
         <div className="flex-1">
