69654675e36765f285081572e8e5151b9167630b
aditri24sn <aditrisurya@gmail.com>
2025-07-22 22:38:57 +0700

    featur: PopUp Fitur

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
-import Header from '../components/Header';
-import Hero from './(pages)/landing/Hero';
-import ExploreSection from './(pages)/landing/ExploreSection';
-import HistoryPreviewSection from './(pages)/landing/HistoryPreviewSection';
-import NewsSection from './(pages)/landing/NewsSection';
-import SOTBSection from './(pages)/landing/SOTBSection';
-import PotentialSection from './(pages)/landing/PotentialSection';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import Hero from "./(pages)/landing/Hero";
+import PopUp from "./(pages)/landing/PopUp";
+import ExploreSection from "./(pages)/landing/ExploreSection";
+import HistoryPreviewSection from "./(pages)/landing/HistoryPreviewSection";
+import NewsSection from "./(pages)/landing/NewsSection";
+import SOTBSection from "./(pages)/landing/SOTBSection";
+import PotentialSection from "./(pages)/landing/PotentialSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
       <main className="bg-gray-100">
+        <PopUp />
         <Hero />
         <ExploreSection />
         <HistoryPreviewSection />
