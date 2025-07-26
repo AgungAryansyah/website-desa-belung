792013ef48141f463040266e645f7e1f8fdf2049
aharismuhasibi <66463824+Hmuhasibi21@users.noreply.github.com>
2025-07-27 01:13:41 +0700

    resolve merge conflict

--- src/app/(pages)/home-berita/page.js
+++ src/app/(pages)/home-berita/page.js
@@ -237,7 +237,7 @@ export default function BeritaPage() {
               {page}
             </button>
             ))}
-
+ 
             <span className="text-gray-400">...</span>
 
             {/* Last Page */}
--- src/app/(pages)/potensi/footerPotensi.js
+++ src/app/(pages)/potensi/footerPotensi.js
@@ -1,14 +1,14 @@
 const FooterPotensi = () => {
   return (
-    <div className="flex flex-col sm:flex-row items-center text-sm text-gray-500 border-t pt-4 sm:pt-6 mt-6 sm:mt-8 gap-4 sm:gap-6">
+    <div className="flex flex-col items-center gap-4 pt-4 mt-6 text-sm text-gray-500 border-t sm:flex-row sm:pt-6 sm:mt-8 sm:gap-6">
       <div className="mb-2 sm:mb-0">Bagikan :</div>{" "}
       {/* Tambah margin bawah untuk mode kolom */}
-      <div className="flex flex-wrap justify-center sm:justify-start gap-4 items-center">
+      <div className="flex flex-wrap items-center justify-center gap-4 sm:justify-start">
         <a href="#" className="transition hover:opacity-80">
           <img
             src="Berita/whatsApp.svg"
             alt="WhatsApp"
-            className="w-7 h-7 sm:w-6 sm:h-6 object-contain"
+            className="object-contain w-7 h-7 sm:w-6 sm:h-6"
           />
         </a>
 
@@ -16,7 +16,7 @@ const FooterPotensi = () => {
           <img
             src="Berita/Facebook.svg"
             alt="Facebook"
-            className="w-7 h-7 sm:w-6 sm:h-6 object-contain"
+            className="object-contain w-7 h-7 sm:w-6 sm:h-6"
           />
         </a>
 
@@ -24,14 +24,14 @@ const FooterPotensi = () => {
           <img
             src="Berita/Instagram.svg"
             alt="Instagram"
-            className="w-7 h-7 sm:w-6 sm:h-6 object-contain"
+            className="object-contain w-7 h-7 sm:w-6 sm:h-6"
           />
         </a>
         <a href="#" className="transition hover:opacity-80">
           <img
             src="Berita/Tiktok.svg"
             alt="Tiktok"
-            className="w-7 h-7 sm:w-6 sm:h-6 object-contain"
+            className="object-contain w-7 h-7 sm:w-6 sm:h-6"
           />
         </a>
         {/* Tambahkan ikon lain jika perlu */}
